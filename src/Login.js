940f015a13b9b9eec1ab45612d0bc9646824022d
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2022-06-25 00:31:52 +0900

    add PageRoutes

---
+++ src/Login.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+/*const Login = () =>{
+
+
+}*/
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         여기는 Login Page 입니다. 시작해볼까요?
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
